b64b72aa0a562d84a54939d9cdb34f4adee085ec
Abhinav2212 <abhinav.mukherjee2212@gmail.com>
2024-06-25 20:10:54 +0530

    ecosystem port added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    apps: [
-      {
-        name: 'nextjs-app',
-        script: 'yarn',
-        args: 'start',
-        env: {
-          NODE_ENV: 'development',
-        },
-        env_production: {
-          NODE_ENV: 'production',
-        },
+  apps: [
+    {
+      name: 'nextjs-app',
+      script: 'yarn',
+      args: 'start',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000, // Specify the port for development
       },
-    ],
-  };
-  
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000, // Specify the port for production
+      },
+    },
+  ],
+};
